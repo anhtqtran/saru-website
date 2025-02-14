8568e25068239a7f200353cc20d6ebf7358d3ef8
unknown <tuttc22411c@st.uel.edu.vn>
2025-02-15 00:06:06 +0700

    Quan ly khuyen mai

--- my-customer/src/app/app-routing.module.ts
+++ my-customer/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KhuyenmaiListComponent } from './khuyenmai-list/khuyenmai-list.component';
+import { KhuyenmaiCreateComponent } from './khuyenmai-create/khuyenmai-create.component';
+import { KhuyenmaiDetailComponent } from './khuyenmai-detail/khuyenmai-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: KhuyenmaiListComponent},
+  { path: 'khuyenmai-create', component: KhuyenmaiCreateComponent },
+  { path: 'khuyenmai-detail', component: KhuyenmaiDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-customer/src/app/app.module.ts
+++ my-customer/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { KhuyenmaiListComponent } from './khuyenmai-list/khuyenmai-list.component';
+import { KhuyenmaiCreateComponent } from './khuyenmai-create/khuyenmai-create.component';
+import { KhuyenmaiDetailComponent } from './khuyenmai-detail/khuyenmai-detail.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KhuyenmaiCreateComponent,
+    KhuyenmaiDetailComponent,
   ],
   imports: [
+    KhuyenmaiListComponent,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
