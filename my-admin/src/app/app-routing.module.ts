a2bb3fabe5bac58d4ada87ce8bbf776bc4644a03
unknown <tuttc22411c@st.uel.edu.vn>
2025-02-14 21:44:59 +0700

    Quan ly don hang

--- my-admin/src/app/app-routing.module.ts
+++ my-admin/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DonhangListComponent } from './donhang-list/donhang-list.component';
+import { DonhangCreateComponent } from './donhang-create/donhang-create.component';
+import { DonhangDetailComponent } from './donhang-detail/donhang-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DonhangListComponent},
+  { path: 'donhang-create', component: DonhangCreateComponent },
+  { path: 'donhang-detail/:id', component: DonhangDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-admin/src/app/app.module.ts
+++ my-admin/src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { DonhangListComponent } from './donhang-list/donhang-list.component';
+import { CommonModule } from '@angular/common';
+import { DonhangCreateComponent } from './donhang-create/donhang-create.component';
+import { DonhangDetailComponent } from './donhang-detail/donhang-detail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonhangCreateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    DonhangDetailComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
