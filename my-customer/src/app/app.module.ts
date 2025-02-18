490a3cae3b612abb7fb0d88235e08d17185fb97c
anhthuuu-u <thuda22411c@st.uel.edu.vn>
2025-02-19 00:48:01 +0700

    add code account manage

--- my-customer/src/app/app.module.ts
+++ my-customer/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountmanageComponent } from './accountmanage/accountmanage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountmanageComponent
   ],
   imports: [
     BrowserModule,
