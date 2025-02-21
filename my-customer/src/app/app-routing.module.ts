17ef1bd1bde501e5b60c3d32399be126c61335ed
unknown <tuttc22411c@st.uel.edu.vn>
2025-02-21 22:37:52 +0700

    Đã gộp component

--- my-customer/src/app/app-routing.module.ts
+++ my-customer/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TrangchuBannerCamketComponent } from './trangchu-banner-camket/trangchu-banner-camket.component';
 
 const routes: Routes = [
-  { path: 'trangchu-banner-camket/trangchu-banner-camket.component', component: TrangchuBannerCamketComponent },
-  { path: 'camketchatluong', component: TrangchuBannerCamketComponent },
+  { path: 'trangchu-banner-camket/trangchu-banner-camket.component', component: TrangchuBannerCamketComponent }
 ];
 
 @NgModule({
