5b0a8c897167c66ccbc7038463d2b0958aa0a2ed
anhthuuu-u <thuda22411c@st.uel.edu.vn>
2025-02-25 22:45:36 +0700

    Merge my-admin từ review-page vào main

--- my-admin/src/app/app.module.ts
+++ my-admin/src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EditproductComponent } from './editproduct/editproduct.component';
-import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EditproductComponent,
-    ProductlistComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
