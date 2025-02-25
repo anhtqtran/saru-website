69be728e58cdca7ea66fbe9a4c6d606d34a6554e
anhthuuu-u <thuda22411c@st.uel.edu.vn>
2025-02-25 22:45:34 +0700

    Merge my-admin từ inventory-manage vào main

--- my-admin/src/app/app.module.ts
+++ my-admin/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventorymanagementComponent } from './inventorymanagement/inventorymanagement.component';
+import { InventorybygroupComponent } from './inventorybygroup/inventorybygroup.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventorymanagementComponent,
+    InventorybygroupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
