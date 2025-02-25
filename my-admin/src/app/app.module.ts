265deadafca6403823cdd7768a579e8208a42075
anhthuuu-u <thuda22411c@st.uel.edu.vn>
2025-02-25 22:45:36 +0700

    Merge my-admin từ product-manage vào main

--- my-admin/src/app/app.module.ts
+++ my-admin/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditproductComponent } from './editproduct/editproduct.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditproductComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
