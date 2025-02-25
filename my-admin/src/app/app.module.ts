6951e389b6215f9385c30651819638e9a81b16dd
anhthuuu-u <thuda22411c@st.uel.edu.vn>
2025-02-25 22:45:35 +0700

    Merge my-admin từ order-manage vào main

--- my-admin/src/app/app.module.ts
+++ my-admin/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InventorymanagementComponent } from './inventorymanagement/inventorymanagement.component';
-import { InventorybygroupComponent } from './inventorybygroup/inventorybygroup.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InventorymanagementComponent,
-    InventorybygroupComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
