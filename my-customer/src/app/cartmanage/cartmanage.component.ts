39e2a3db784712fc959b561c8fd1d6f1c0a7ffe9
anhthuuu-u <thuda22411c@st.uel.edu.vn>
2025-03-01 00:59:34 +0700

    fix code cart manage

--- my-customer/src/app/cartmanage/cartmanage.component.ts
+++ my-customer/src/app/cartmanage/cartmanage.component.ts
@@ -7,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrl: './cartmanage.component.css'
 })
 export class CartmanageComponent {
+  cartItems = [
+    {
+      id: 1,
+      name: 'Rượu mơ má đào Mộc Châu',
+      price: 279000,
+      quantity: 1,
+      image: 'product-placeholder.png'
+    }
+  ];
 
+  showPopup: boolean = false; // Kiểm soát trạng thái hiển thị popup
+
+  increaseQuantity(index: number) {
+    this.cartItems[index].quantity++;
+  }
+
+  decreaseQuantity(index: number) {
+    if (this.cartItems[index].quantity > 1) {
+      this.cartItems[index].quantity--;
+    }
+  }
+
+  removeItem(index: number) {
+    this.cartItems.splice(index, 1);
+  }
+
+  getTotalPrice(): number {
+    return this.cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  }
+
+  placeOrder() {
+    this.showPopup = true; // Mở popup khi đặt hàng thành công
+  }
+
+  closePopup() {
+    this.showPopup = false; // Đóng popup khi nhấn "Trở lại"
+  }
 }
