3f0054e036c0b456a63a75c085b32f7439e14c7f
Anh Tran <anh.tq.tran@gmail.com>
2025-03-16 07:28:45 +0700

    done merge của My

--- my-admin/src/app/app.module.ts
+++ my-admin/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
   
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
   ],
   imports: [
     BrowserModule,
